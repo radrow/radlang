4dd21a0d25fe2620a9d58cabe3b514ad116395b8
radrow <radrowicki@gmail.com>
2018-11-13 15:06:06 +0100

    Better REPL

--- rdli/Main.hs
+++ rdli/Main.hs
@@ -1,20 +1,52 @@
+{-#LANGUAGE LambdaCase#-}
 module Main where
 
+import Data.Char
+import Data.List(find, isPrefixOf)
 import Control.Monad
+import System.Exit
 import System.IO
 
 import Radlang
 
+data Action
+  = Eval
+  | Typecheck
+  | Quit
+
+commandMap :: [(String, Action)]
+commandMap =
+  [ (":t", Typecheck)
+  , (":type", Typecheck )
+  , (":q", Quit)
+  , (":quit", Quit)
+  ]
+
+processLine :: String -> (Action, String)
+processLine l = case find (\(s, _) -> isPrefixOf s l) commandMap of
+  Just (s, a) -> (a, dropWhile (isSpace) $ drop (length s) l)
+  Nothing -> (Eval, dropWhile (isSpace) l)
+
+printResult :: Either ErrMsg String -> IO ()
+printResult = \case
+  Right r -> putStrLn r
+  Left e -> hPutStrLn stderr e
+
 main :: IO ()
 main = forever $ do
-  putStr "RDL: "
-  hFlush stdout
-  line <- getLine
-  let result = do
+  hPutStr stderr "RDL: "
+  hFlush stderr
+  lineRead <- getLine
+  let (action, line) = processLine lineRead
+  unless (all isSpace lineRead) $
+    case action of
+      Eval -> printResult $ do
+        e <- parseProgram "interactive" line
+        void $ typecheck e
+        d <- evalProgram e
+        pure $ show d
+      Typecheck -> printResult $ do
         e <- parseProgram "interactive" line
         t <- typecheck e
-        d <- evalProgram e
-        pure (t, d)
-  case result of
-    Left e -> hPutStrLn stderr e
-    Right (t, d) -> putStrLn $ show d <> " : " <> show t
+        pure $ show t
+      Quit -> hPutStrLn stderr "Bye!" >> exitSuccess
