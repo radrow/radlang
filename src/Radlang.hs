8d66df4a38e26648b6603db74ad1fa9a8b74ab1c
radrow <radrowicki@gmail.com>
2018-11-13 12:48:31 +0100

    Radlang.hs comments

--- src/Radlang.hs
+++ src/Radlang.hs
@@ -1,17 +1,21 @@
+-- |Purely functional programming language inspired by Haskell and OCaml
+
 module Radlang
     ( parseProgram
     , module Radlang.Types
     , module Radlang.Evaluator
     , module Radlang.Typechecker
+    , module Radlang.Parser
     ) where
 
 import Data.Bifunctor(bimap)
 import Text.Megaparsec as MP(parse, parseErrorPretty, eof)
 
-import Radlang.Parser(ast, processAST)
+import Radlang.Parser
 import Radlang.Evaluator
 import Radlang.Types
 import Radlang.Typechecker
 
+-- |Toplevel parser run with desugaring
 parseProgram :: String -> String -> Either String Expr
 parseProgram filename code = bimap parseErrorPretty processAST $ MP.parse (ast <* eof) filename code
