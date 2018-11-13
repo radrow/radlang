c18b939f4bab2a49a56c3dc4082462f5aa2b5d7e
radrow <radrowicki@gmail.com>
2018-11-13 10:46:46 +0100

    Removed EvaluationEnv

--- src/Radlang/Evaluator.hs
+++ src/Radlang/Evaluator.hs
@@ -9,7 +9,64 @@ import qualified Data.Map.Strict as M
 
 import Radlang.Helpers
 import Radlang.Types
-import Radlang.EvaluationEnv
+import Radlang.Stdlib
+
+
+getNamespace :: Evaluator Namespace
+getNamespace = lift ask
+
+getDataspace :: Evaluator Dataspace
+getDataspace = lift $ lift get
+
+setDataspace :: Dataspace -> Evaluator ()
+setDataspace d = lift $ lift $ put d
+
+-- |Insert multiple assignments to namespace
+inserts :: [(Name, Int)] -> Namespace -> Namespace
+inserts as ns = foldl (flip $ uncurry M.insert) ns as
+
+-- |Update namespace with new set of names replacing previous bindings
+update :: Namespace -> Namespace -> Namespace
+update = M.union
+
+-- |Try to get data by name
+lookupName :: Name -> Evaluator (Maybe Data)
+lookupName n = do
+  ns <- getNamespace
+  ds <- fst <$> getDataspace
+  case M.lookup n ns of
+    Just i -> case M.lookup i ds of
+      Just d -> pure $ Just d
+      Nothing -> throwError $ "Unbound id: " <> show i
+    Nothing -> pure Nothing
+
+
+-- |Allocates new data and returns id
+registerData :: Data -> Evaluator DataId
+registerData d = do
+  (ds, count) <- getDataspace
+  put $ (M.insert (count + 1) d ds, count + 1)
+  pure $ count + 1
+
+-- |Evals with overbound variable id
+withAssg :: (Name, Int) -> Evaluator a -> Evaluator a
+withAssg (n, d) = local (M.insert n d)
+
+-- |Evals with data bound to name
+withData :: (Name, Data) -> Evaluator a -> Evaluator a
+withData (n, d) e = registerData d >>= \i -> withAssg (n <~ i) e
+
+-- |Evals with updated namespace
+withNs :: Namespace -> Evaluator a -> Evaluator a
+withNs n = local (update n)
+
+-- |Evals with stdlib included
+withStdlib :: Evaluator a -> Evaluator a
+withStdlib ev = do
+  ns <- fmap M.fromList $ forM stdlib $ \(name, val ::: _) -> do
+    i <- registerData val
+    pure (name <~ i)
+  withNs ns ev
 
 -- |Same as `withAssg`, but evaluation performed
 withAssgExpr :: (Name, Int) -> Expr -> Evaluator Data
