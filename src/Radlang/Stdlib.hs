1484892c2ced5b31a612708494a3824593658717
radrow <radrowicki@gmail.com>
2018-11-16 11:39:51 +0100

    Stdlib – negations

--- src/Radlang/Stdlib.hs
+++ src/Radlang/Stdlib.hs
@@ -6,7 +6,8 @@ module Radlang.Stdlib(stdlib, Prim(..)) where
 import Radlang.Types
 import Radlang.Helpers
 
-import Prelude hiding (or, and)
+import Prelude hiding (or, and, not)
+import qualified Prelude
 
 data Prim where
   (:::) :: Data -> TypePoly -> Prim
@@ -26,6 +27,8 @@ divide = fun (\(DataInt i) -> fun (\(DataInt j) -> DataInt (i `div` j)))
 
 eq :: Data
 eq = fun (\a -> fun (\b -> DataBool $ a == b))
+neq :: Data
+neq = fun (\a -> fun (\b -> DataBool $ a /= b))
 
 geq :: Data
 geq = fun (\(DataInt i) -> fun (\(DataInt j) -> DataBool (i >= j)))
@@ -40,6 +43,8 @@ or :: Data
 or = fun (\(DataBool i) -> fun (\(DataBool j) -> DataBool (i || j)))
 and :: Data
 and = fun (\(DataBool i) -> fun (\(DataBool j) -> DataBool (i && j)))
+not :: Data
+not = fun (\(DataBool i) -> DataBool (Prelude.not i))
 
 
 stdlib :: [(Name, Prim)]
@@ -49,10 +54,12 @@ stdlib =
   , "mult" <~ mult ::: makePoly (TypeFunc TypeInt (TypeFunc TypeInt TypeInt))
   , "div" <~ divide ::: makePoly (TypeFunc TypeInt (TypeFunc TypeInt TypeInt))
   , "eq" <~ eq ::: Poly (pure "A") (TypeFunc (TypeVal "A") (TypeFunc (TypeVal "A") TypeBool))
+  , "neq" <~ neq ::: Poly (pure "A") (TypeFunc (TypeVal "A") (TypeFunc (TypeVal "A") TypeBool))
   , "geq" <~ geq ::: makePoly (TypeFunc TypeInt (TypeFunc TypeInt TypeBool))
   , "gt" <~ gt ::: makePoly (TypeFunc TypeInt (TypeFunc TypeInt TypeBool))
   , "leq" <~ leq ::: makePoly (TypeFunc TypeInt (TypeFunc TypeInt TypeBool))
   , "lt" <~ lt ::: makePoly (TypeFunc TypeInt (TypeFunc TypeInt TypeBool))
   , "or" <~ or ::: makePoly (TypeFunc TypeBool (TypeFunc TypeBool TypeBool))
   , "and" <~ and ::: makePoly (TypeFunc TypeBool (TypeFunc TypeBool TypeBool))
+  , "not" <~ not ::: makePoly (TypeFunc TypeBool TypeBool)
   ]
