538cc0c833477c2519e144ba1145b81f05f5851d
radrow <radrowicki@gmail.com>
2018-11-26 22:05:36 +0100

    Half of new typesystem

--- src/Radlang/Typechecker.hs
+++ src/Radlang/Typechecker.hs
@@ -1,9 +1,13 @@
+{-#LANGUAGE MultiWayIf #-}
+{-#LANGUAGE OverloadedLists #-}
 -- |Implementation of the W Algorithm for typechecking
 
 module Radlang.Typechecker(typecheck) where
 
+import Data.List
 import Data.Map.Strict as M
 import Data.Set.Monad as S
+import Control.Applicative
 import Control.Monad.State.Strict
 import Control.Monad.Reader
 import Control.Monad.Except
@@ -12,17 +16,140 @@ import Radlang.Types
 import Radlang.Stdlib
 import Radlang.Helpers
 
+
+-- |Gets superclasses of class by name
+super :: ClassEnv -> Name -> Typechecker (Set Name)
+super ce n = case M.lookup n (classes ce) of
+  Just (is, _) -> pure is
+  Nothing -> throwError $ "superclass lookup: " <> n <> " not defined"
+
+
+-- |Gets instances of class by name
+instances :: ClassEnv -> Name -> Typechecker (Set Inst)
+instances ce n = case M.lookup n (classes ce) of
+  Just (_, its) -> pure its
+  Nothing -> throwError $ "instances lookup: " <> n <> " not defined"
+
+
+updateClassEnv :: ClassEnv -> Name -> Class -> ClassEnv
+updateClassEnv ce n c = ce {classes = M.insert n c (classes ce)}
+
+
+emptyClassEnv :: ClassEnv
+emptyClassEnv = ClassEnv
+  { classes = []
+  , defaults = S.fromList [TypeInt]
+  }
+
+
+-- |Merge substitution ensuring that they agree
+merge :: Substitution -> Substitution -> Typechecker Substitution
+merge s1 s2 =
+  let extract (n, t) = TypeVar n (kind t)
+      agree = all (liftA2 (==) (substitute s1 . TypeVarWobbly) (substitute s2 .TypeVarWobbly))
+        (fmap extract $ M.toList (getSubstMap s1) `intersect` M.toList (getSubstMap s2))
+  in if agree then pure (s1 <> s2) else throwError "Cannot merge substitutions"
+
+
+classDefined :: ClassEnv -> Name -> Bool
+classDefined ce n = M.member n $ classes ce
+
+
+-- |Introduces new class extending given superclasses
+addClass :: Name -> Set Name -> ClassEnv -> Typechecker ClassEnv
+addClass n sups ce = do
+  when (classDefined ce n) (throwError $ "Class already defined: " <> n)
+  case find (not . classDefined ce) (S.toList sups) of
+    Nothing -> pure ()
+    Just delikwent -> throwError $ "Superclass not defined: " <> delikwent
+  pure $ updateClassEnv ce n (sups, S.empty)
+
+
+-- |Declares new instance
+addInst :: Set Pred -> Pred -> ClassEnv -> Typechecker ClassEnv
+addInst ps p@(IsIn i _) ce = do
+  when (not $ classDefined ce i) (throwError $ "Class not defined: " <> i)
+  its <- instances ce i
+  c <- super ce i
+  let overlaps p q = catchError (mguPred p q >> pure True) (const $ pure False)
+      qs = fmap (\(_ :=> q) -> q) its
+  filterM (overlaps p) (S.toList qs) >>= \case
+    [] -> pure ()
+    (IsIn h _):_ -> throwError $ "Instances overlap: " <> i <> " with " <> h
+  pure $ updateClassEnv ce i (c, S.insert (ps :=> p) its)
+
+
+-- |Deep search for all superclasses
+bySuper :: ClassEnv -> Pred -> Typechecker (Set Pred)
+bySuper ce p@(IsIn i t) = do
+  i' <- S.toList <$> super ce i
+  if Prelude.null i'
+    then pure $ S.singleton p
+    else S.insert p <$> (S.unions <$> (forM i' (\i'' -> bySuper ce (IsIn i'' t))))
+
+
+byInst :: ClassEnv -> Pred -> Typechecker (Set Pred)
+byInst ce p@(IsIn i t) = do -- TODO ensure if it works
+  let tryInst (ps :=> h) = do
+        u <- matchPred h p
+        pure $ fmap (substitute u) ps
+  insts <- S.toList <$> instances ce i
+  let its :: [Typechecker (Set Pred)]
+      its = flip fmap insts $ \it -> tryInst it
+  msum its
+
+
+entail :: ClassEnv -> Set Pred -> Pred -> Typechecker Bool
+entail ce ps p = do
+  sups <- forM (S.toList ps) $ \pp -> bySuper ce pp
+  let instCheck = do
+        sups <- traverse (bySuper ce) (S.toList ps)
+        pure $ any (elem p) sups
+  inst <- catchError instCheck (const $ pure False)
+  pure $ any (elem p) sups || inst
+
+
+inHNF :: Pred -> Bool
+inHNF (IsIn _ t) = case t of
+  (TypeVarWobbly v) -> True
+  (TypeVarRigid v) -> False
+  (TypeApp tt _) -> inHNF (IsIn undefined tt)
+  _ -> error "unimplemented ihnf"
+
+
+toHNF :: ClassEnv -> Pred -> Typechecker (Set Pred)
+toHNF ce p =
+  if inHNF p
+    then pure $ S.singleton p
+    else byInst ce p >>= toHNFs ce
+
+
+toHNFs :: ClassEnv -> Set Pred -> Typechecker (Set Pred)
+toHNFs = do pss <- mapM (toHNF ce) ps
+            pure $ join pss
+
+
+-- simplify is built in
+
+
+reduce :: ClassEnv -> Set Pred -> Typechecker (Set Pred)
+reduce = toHNFs
+
+
 -- |Lookup type in typespace
 lookupType :: Name -> Typechecker (Maybe TypePoly)
 lookupType n = M.lookup n . getTypespaceMap <$> getTypespace
+
 
 -- |Returns typespace
 getTypespace :: Typechecker Typespace
 getTypespace = ask
 
+
 -- |Runs Typechecker with another typespace
 withTypespace :: Typespace -> Typechecker a -> Typechecker a
 withTypespace ts = local (const ts)
+
 
 -- |Runs Typechecker with additional type assignment
 withTypeAssg :: (Name, Type) -> Typechecker a -> Typechecker a
@@ -31,9 +158,11 @@ withTypeAssg (n, t) tc = do
   let newts = Typespace . (M.insert n (Poly S.empty t)) . getTypespaceMap $ ts
   withTypespace newts tc
 
+
 -- |Substitutes variables in typespace of given Typechecker
 withSubstitution :: Substitution -> Typechecker a -> Typechecker a
 withSubstitution s tc = ask >>= \t -> withTypespace (substitute s t) tc
+
 
 -- |Runs Typecheker with standard library
 withStdlib :: Typechecker a -> Typechecker a
@@ -41,17 +170,20 @@ withStdlib tc = do
   let ts = Typespace $ M.fromList $ flip fmap stdlib $ \(name, _ ::: typ) -> (name <~ typ)
   withTypespace ts tc
 
+
 -- |Abstracts out types that are free in t but not in typespace
 generalize :: Typespace -> Type -> TypePoly
 generalize ts t = Poly vars t where
   vars = free t S.\\ free ts
 
+
 -- |Returns new type variable
-newVar :: String -> Typechecker Type
-newVar prefix = do
+newVar :: String -> Kind -> Typechecker Type
+newVar prefix k = do
   c <- gets tsSupply
   modify $ \s -> s{ tsSupply = c + 1 }
-  pure $ TypeVal $ prefix <> show c
+  pure $ TypeVarWobbly $ TypeVar (prefix <> show c) k
+
 
 -- |Replace all bound typevars in scheme with fresh typevars
 instantiate :: TypePoly -> Typechecker Type
@@ -60,33 +192,70 @@ instantiate (Poly vars t) = do
   let newsub = Subst $ M.fromList $ zip (S.toList vars) nvars
   pure $ substitute newsub t
 
+
 -- |Creates substitution with type assigned
-bindVar :: Name -> Type -> Typechecker Substitution
-bindVar n t = case t of
-  TypeVal v | v == n -> pure mempty
-  _ -> if S.member n (free t)
-          -- Occur check
-       then throwError $ "Cannot create infinite type: " <> n <> " := " <> show t
-       else pure $ Subst $ M.singleton n t
+bindVar :: TypeVar -> Type -> Typechecker Substitution
+bindVar tv t = if
+    | t == TypeVarWobbly tv -> pure mempty
+    | S.member (tName tv) (free t) ->
+      throwError $ "Occur check: cannot create infinite type: " <> tName tv <> " := " <> show t
+    | kind tv /= kind t -> throwError $ "Kinds don't match"
+    | otherwise -> pure $ Subst $ M.singleton (tName tv) t
+
 
 -- |Most general unifier
 mgu :: Type -> Type -> Typechecker Substitution
 mgu t1 t2 = case (t1, t2) of
   (TypeInt, TypeInt) -> pure mempty
-  (TypeBool, TypeBool) -> pure mempty
-  (TypeVal n, _) -> bindVar n t2
-  (_, TypeVal n) -> bindVar n t1
-  (TypeValRigid a, TypeValRigid b) ->
+  (TypeVarWobbly tv, _) -> bindVar tv t2
+  (_, TypeVarWobbly tv) -> bindVar tv t1
+  (TypeApp f1 a1, TypeApp f2 a2) -> do
+    sf <- mgu f1 f2
+    sa <- mgu a1 a2
+    pure $ sa <> sf
+  (TypeVarRigid a, TypeVarRigid b) ->
     if a == b
     then pure mempty
-    else throwError $ "Cannot unify rigid different type variables: " <> a <> " vs " <> b
-  (TypeFunc a1 v1, TypeFunc a2 v2) -> do
-    sa <- mgu a1 a2
-    sv <- mgu (substitute sa v1) (substitute sa v2)
-    pure $ sa <> sv
-  (TypeValRigid a, b) -> throwError $ "Cannot unify rigid type variable with non-rigid type: " <> a <> " vs " <> show b
-  (b, TypeValRigid a) -> throwError $ "Cannot unify rigid type variable with non-rigid type: " <> show b <> " vs " <> a
+    else throwError $ "Cannot unify rigid different type variables: " <> tName a <> " vs " <> tName b
+  (TypeVarRigid (TypeVar a _), b) ->
+    throwError $ "Cannot unify rigid type variable with non-rigid type: " <> a <> " vs " <> show b
+  (b, TypeVarRigid (TypeVar a _)) ->
+    throwError $ "Cannot unify rigid type variable with non-rigid type: " <> show b <> " vs " <> a
   _ -> throwError $ "Cannot unify types: " <> show t1 <> " vs " <> show t2
+
+
+-- |Unifier that uses `merge` instead of `<>`
+match :: Type -> Type -> Typechecker Substitution
+match t1 t2 = case (t1, t2) of
+  (TypeInt, TypeInt) -> pure mempty
+  (TypeApp f1 a1, TypeApp f2 a2) -> do
+    sf <- mgu f1 f2
+    sa <- mgu a1 a2
+    merge sa sf
+  (TypeVarWobbly tv, t) | kind tv == kind t -> bindVar tv t2
+  (TypeVarRigid a, TypeVarRigid b) ->
+    if a == b
+    then pure mempty
+    else throwError $ "Cannot merge rigid different type variables: " <> tName a <> " vs " <> tName b
+  (TypeVarRigid (TypeVar a _), b) ->
+    throwError $ "Cannot merge rigid type variable with non-rigid type: " <> a <> " vs " <> show b
+  (b, TypeVarRigid (TypeVar a _)) ->
+    throwError $ "Cannot merge rigid type variable with non-rigid type: " <> show b <> " vs " <> a
+  _ -> throwError $ "Cannot merge types: " <> show t1 <> " vs " <> show t2
+
+
+mguPred :: Pred -> Pred -> Typechecker Substitution
+mguPred (IsIn i1 t1) (IsIn i2 t2) =
+  if i1 == i2 then mgu t1 t2
+  else throwError $ "Classes don't unify: " <> i1 <> " vs " <> i2
+
+
+matchPred :: Pred -> Pred -> Typechecker Substitution
+matchPred (IsIn i1 t1) (IsIn i2 t2) =
+  if i1 == i2 then match t1 t2
+  else throwError $ "Classes don't match: " <> i1 <> " vs " <> i2
+
+
 
 -- |Type inference along with check
 inferType :: Expr -> Typechecker (Substitution, Type)
--- src/Radlang/Types/Typesystem.hs
+++ src/Radlang/Types/Typesystem.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE TypeOperators #-}
+
 -- |Types related to typechecking
 
 module Radlang.Types.Typesystem where
@@ -12,15 +14,18 @@ import Data.Set.Monad(Set)
 
 import Radlang.Types.General
 
+
 ---- DEFINITIONS ----
 
 -- |Typechecker state currently contains only count of runtime generated types
 data TypecheckerState = TypecheckerState { tsSupply :: Int}
   deriving (Eq, Show)
 
+
 -- |Map of typenames into real types
 newtype Typespace = Typespace { getTypespaceMap :: Map Name TypePoly }
   deriving (Eq, Show, Ord)
+
 
 -- |Transformer responsible for typechecking expressions and error handling
 type Typechecker = ExceptT String (ReaderT Typespace (State TypecheckerState))
@@ -28,20 +33,48 @@ type Typechecker = ExceptT String (ReaderT Typespace (State TypecheckerState))
 
 -- |Primitive type definition
 data Type
-  = TypeVal Name
-  | TypeValRigid Name
+  = TypeVarWobbly TypeVar -- Inferred type variable
+  | TypeVarRigid TypeVar -- User specified polymorphic type
   | TypeInt
-  | TypeBool
-  | TypeFunc Type Type
+  | TypeADT Int -- ADT with given arity
+  | TypeApp Type Type
   deriving (Eq, Ord)
+
+data TypeVar = TypeVar {tName :: Name, tKind :: Kind}
+  deriving (Eq, Show, Ord)
+
+data Kind = KType | KFunc Kind Kind
+  deriving (Eq, Show, Ord)
+
 
 -- |Type served along with polymorphic names used inside
 data TypePoly = Poly (Set Name) Type
   deriving (Eq, Show, Ord)
 
+
+data Pred = IsIn Name Type
+  deriving (Eq, Ord)
+
+
+data Qual t = Set Pred :=> t
+  deriving (Eq, Ord)
+
+
+type Inst = Qual Pred
+
+
+type Class = (Set Name, Set Inst)
+
+
+data ClassEnv = ClassEnv { classes :: Map Name Class
+                         , defaults :: Set Type
+                         }
+
+
 -- |Substitution of polymorphic types
 newtype Substitution = Subst { getSubstMap :: Map Name Type }
   deriving (Eq, Show, Ord)
+
 
 -- |Types that may be considered as free types carriers
 class Ord t => IsType t where -- Ord is needed because use of Set
@@ -51,56 +84,85 @@ class Ord t => IsType t where -- Ord is needed because use of Set
   substitute :: Substitution -> t -> t
 
 
+class HasKind t where
+  kind :: t -> Kind
+
+
 ---- INSTANCES ----
 
 instance IsType t => IsType (Set t) where
   free s = s >>= free
   substitute s = S.map (substitute s)
 
+
 instance IsType Type where
   free = \case
     TypeInt -> S.empty
-    TypeBool -> S.empty
-    TypeFunc a v -> S.union (free a) (free v)
-    TypeVal v -> S.singleton v
-    TypeValRigid v -> S.singleton v
+    TypeApp a v -> S.union (free a) (free v)
+    TypeVarWobbly (TypeVar v _) -> S.singleton v
+    TypeVarRigid (TypeVar v _) -> S.singleton v
+    TypeADT _ -> undefined
   substitute s@(Subst sm) = \case
     TypeInt -> TypeInt
-    TypeBool -> TypeBool
-    TypeFunc a v -> TypeFunc (substitute s a) (substitute s v)
-    TypeVal n -> case M.lookup n sm of
+    TypeApp a v -> TypeApp (substitute s a) (substitute s v)
+    TypeVarWobbly (TypeVar n k) -> case M.lookup n sm of
       Just t -> t
-      Nothing -> TypeVal n
-    TypeValRigid n -> case M.lookup n sm of
+      Nothing -> TypeVarWobbly $ TypeVar n k
+    TypeVarRigid (TypeVar n k) -> case M.lookup n sm of
       Just t -> t
-      Nothing -> TypeValRigid n
+      Nothing -> TypeVarRigid $ TypeVar n k
+    TypeADT _ -> undefined
+
+
+instance IsType Pred where
+  free (IsIn _ t) = free t
+  substitute s (IsIn i t) = IsIn i (substitute s t)
+
+
+instance IsType t => IsType (Qual t) where
+  free (ps :=> t) = free ps `S.union` free t
+  substitute s (ps :=> t) = substitute s ps :=> substitute s t
+
 
 instance IsType TypePoly where
   free (Poly vars t) = free t S.\\ vars
   substitute (Subst s) (Poly vars t) =
     Poly vars $ substitute (Subst $ foldr M.delete s vars) t
 
+
 instance IsType Typespace where
   free (Typespace ts) = free $ S.fromList (M.elems ts)
   substitute s (Typespace ts) = Typespace $
     M.map (substitute s) ts
 
+
 instance Semigroup Substitution where
   (<>) s@(Subst s1) (Subst s2) =
     Subst $ M.map (substitute s) s2 `M.union` s1
 
+
 instance Monoid Substitution where
   mempty = Subst M.empty
 
+
 instance Show Type where
   show = \case
-    TypeVal a -> a
-    TypeValRigid a -> a
+    TypeVarWobbly (TypeVar a _) -> a
+    TypeVarRigid (TypeVar a _) -> a
     TypeInt -> "Int"
-    TypeBool -> "Bool"
-    TypeFunc a v ->
-      let sa = case a of
-            TypeFunc _ _ -> "(" <> show a <> ")"
-            _ -> show a
-      in sa <> " -> " <> show v
+    TypeApp a b -> show a <> " " <> show b
+    TypeADT _ -> undefined
 
+
+instance HasKind Type where
+  kind (TypeVarWobbly tv) = kind tv
+  kind (TypeVarRigid tv) = kind tv
+  kind TypeInt = KType
+  kind (TypeADT _) = undefined
+  kind (TypeApp f _) = case kind f of
+    (KFunc _ k) -> k
+    _ -> error "Kindcheck failed"
+
+
+instance HasKind TypeVar where
+  kind (TypeVar _ k) = k
